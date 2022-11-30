12baf601172bb7bc0bdf6719d61a706a7bc7a30f
Tommi Prusti <tommi.prusti@tuni.fi>
2022-11-30 02:51:31 +0200

    Start of the assigment part 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ohjelmistotestaus_groupwork
+This is repository for COMP.SE.200 groupwork. Here will be found codes and tests which were done in the part 1 of the assignment.
---
+++ test/add.test.js
@@ -0,0 +1,18 @@
+import chai from "chai"
+import add from "../src/add.js"
+const expect = chai.expect
+
+
+describe("Tests for add function", () => {
+ it("Add positive number to positive number", () =>{
+     expect(add(5,5)).to.equal(10)
+ });
+ it("Add positive number to negative number", () =>{
+    expect(add(5,-7)).to.equal(-2)
+ });
+ it("Should throw TypeError if argument is not a number", () =>{
+    expect(function() {
+        add(5,"s");
+    }).to.throw(TypeError);
+ });
+})
