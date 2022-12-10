c9323057888df4e463de1bb9cfa830d9950e0a77
Emma Syynimaa <emma.syynimaa@tuni.fi>
2022-12-10 12:26:29 +0200

    added drop test

---
+++ test/drop.test.js
@@ -0,0 +1,19 @@
+import chai from "chai"
+import drop from "../src/drop.js"
+const expect = chai.expect
+
+
+describe("Tests for drop function", () => {
+    it("should drop first element from the given list", () =>{
+        expect(drop(['maito', 'piima', 'juusto'],1)).to.eql(['piima', 'juusto'])
+    });
+
+    it("should drop all elements from the given list", () =>{
+        expect(drop(['maito', 'piima', 'juusto'],3)).to.eql([])
+    });
+
+    it("should return empty list with not valid arguments", () =>{
+        expect(drop(3,"maito")).to.eql([])
+    });
+
+})
